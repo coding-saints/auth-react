334904d837bde1b65e3f050bcaab00256b62cc05
Nick Kaufmann <coding.saints2015@gmail.com>
2017-03-29 21:37:23 -0400

    initial skeleton for node/webpack setup

---
+++ client/src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+render(
+    <h1>this is runnin react</h1>,
+    document.getElementById('react-app'));
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+
+module.exports = {
+  // the entry file for the bundle
+  entry: path.join(__dirname, '/client/src/app.js'),
+
+  // the bundle file we will get in the result
+  output: {
+    path: path.join(__dirname, '/client/dist/js'),
+    filename: 'app.js',
+  },
+
+ resolve: {
+    extensions: ['.js', '.jsx', '.json', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"]
+      }
+    ]
+  },
+
+  // start Webpack in a watch mode, so Webpack will rebuild the bundle on changes
+  watch: true
+};
